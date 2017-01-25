a53ae1217879d99ecb084025c024c7979be8a3de
Hari Prasad <dnshariprasad@gmail.com>
2017-01-25 16:13:34 +0530

    initial commit

---
+++ app/src/main/java/com/meaningful_ui_motion/PictureView.java
@@ -0,0 +1,42 @@
+package com.meaningful_ui_motion;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by Hari Prasad on 1/25/17.
+ */
+
+public class PictureView extends ImageView {
+
+    private int mResId;
+
+    public PictureView(Context context) {
+        this(context, null, 0);
+    }
+
+    public PictureView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public PictureView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        if (attrs != null) {
+            final String namespace = "http://schemas.android.com/apk/res/android";
+            final String attribute = "src";
+            mResId = attrs.getAttributeResourceValue(namespace, attribute, 0);
+        }
+    }
+
+    public int getImageResource() {
+        return mResId;
+    }
+
+    @Override
+    public void setImageResource(int resId) {
+        super.setImageResource(resId);
+        mResId = resId;
+    }
+}
